5328f669417f7dc409a9e791139df911b894b1c6
Bogdan <bogdan.terebin@ya.ru>
2023-11-27 01:00:08 +0400

    iadd imul isub itruediv

--- magic_methods.py
+++ magic_methods.py
@@ -405,4 +405,61 @@ class SuperString:
         return NotImplemented
     
 
+class Time:
+    def __init__(self, hours, minutes):
+        self.hours = (hours + minutes // 60) % 24
+        self.minutes = minutes % 60
 
+    def __str__(self):
+        return f'{self.hours:02}:{self.minutes:02}'
+
+    def __add__(self, other):
+        if isinstance(other, Time):
+            return Time(self.hours + other.hours, self.minutes + other.minutes)
+        return NotImplemented
+    
+    def __iadd__(self, other):
+        if not isinstance(other, Time):
+            return NotImplemented
+        self.hours = (self.hours + other.hours + (self.minutes + other.minutes) // 60) % 24
+        self.minutes = (self.minutes + other.minutes) % 60
+        return self
+
+
+from collections import deque
+from itertools import islice
+
+class Queue:
+    def __init__(self, *args):
+        self.queue = deque(args)
+
+    def add(self, *args):
+        self.queue.extend(args)
+
+    def pop(self):
+        if len(self.queue):
+            return self.queue.popleft()
+    
+    def __str__(self):
+        return ' -> '.join(map(str, self.queue))
+    
+    def __eq__(self, other):
+        if isinstance(other, Queue):
+            return self.queue == other.queue
+        return NotImplemented
+    
+    def __add__(self, other):
+        if isinstance(other, Queue):
+            return Queue(*self.queue, *other.queue)
+        return NotImplemented
+    
+    def __iadd__(self, other):
+        if isinstance(other, Queue):
+            self.queue.extend(other.queue)
+            return self
+        return NotImplemented
+    
+    def __rshift__(self, other):
+        if isinstance(other, int):
+            return Queue(*islice(self.queue, other, None))
+        return NotImplemented
