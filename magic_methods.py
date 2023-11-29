7f4a3424e7c6ccb296f88dbf8b54c3adf94834ef
Bogdan <bogdan.terebin@ya.ru>
2023-11-29 21:07:12 +0400

    getattr getattribute setattr delattr

--- magic_methods.py
+++ magic_methods.py
@@ -52,6 +52,7 @@ class Vector:
     
 
 from functools import singledispatchmethod
+from typing import Any
 
 class IPAddress:
     @singledispatchmethod
@@ -683,3 +684,95 @@ class RomanNumeral:
     
     def __sub__(self, other):
         return RomanNumeral(self._to_roman(int(self) - int(other)))
+    
+
+class Item:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def __getattribute__(self, name):
+        if name == 'total': 
+            return  object.__getattribute__(self, 'price') *  object.__getattribute__(self, 'quantity')
+        elif name == 'name':
+            return  object.__getattribute__(self, 'name').title()
+        return object.__getattribute__(self, name)
+    
+
+class Logger:
+    def __setattr__(self, name, value):
+        print(f'Изменение значения атрибута {name} на {value}')
+        object.__setattr__(self, name, value)
+
+    def __delattr__(self, name):
+        print(f'Удаление атрибута {name}')
+        object.__delattr__(self, name)
+
+
+class Ord():
+    def __getattr__(self, attr):
+        return ord(attr)
+    
+
+class DefaultObject:
+    def __init__(self, default=None, **kwargs):
+        self.default = default
+        self.__dict__ |= kwargs
+
+    def __getattr__(self, name):
+        return self.default
+    
+
+class NonNegativeObject:
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            self.__setattr__(k, v)
+
+    def __setattr__(self, name, value):
+        if isinstance(value, (float, int)) and value < 0:
+            value *= -1
+        object.__setattr__(self, name, value)
+
+
+class AttrsNumberObject:
+    def __init__(self, **kwargs):
+        self.__dict__ |= kwargs
+
+    @property
+    def attrs_num(self):
+        return len(self.__dict__) + 1
+    
+
+class Const:
+    def __init__(self, **kwargs):
+        self.__dict__ |= kwargs
+
+    def __setattr__(self, name, value):
+        if name in self.__dict__:
+            raise AttributeError('Изменение значения атрибута невозможно')
+        object.__setattr__(self, name, value)
+
+    def __delattr__(self, name):
+        raise AttributeError('Удаление атрибута невозможно')
+    
+
+class ProtectedObject:
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            object.__setattr__(self, k, v)
+
+    def __getattribute__(self, name):
+        if name.startswith('_'):
+            raise AttributeError('Доступ к защищенному атрибуту невозможен')
+        return object.__getattribute__(self, name)
+    
+    def __setattr__(self, name, value):
+        if name.startswith('_'):
+            raise AttributeError('Доступ к защищенному атрибуту невозможен')
+        return object.__setattr__(self, name, value)
+    
+    def __delattr__(self, name): 
+        if name.startswith('_'):
+            raise AttributeError('Доступ к защищенному атрибуту невозможен')
+        object.__delattr__(self, name)
