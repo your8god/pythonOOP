83dcce54a347f1d273e28245d1b2215d195aabce
Богдан <bogdan.terebin@ya.ru>
2023-12-16 14:51:45 +0400

    set get set_name delete

--- protocols.py
+++ protocols.py
@@ -766,4 +766,50 @@ class RandomNumber:
     
     def __set__(self, obj, val):
         raise AttributeError('Изменение невозможно')
-        
+
+
+class Versioned:
+    def __set_name__(self, cls, attr):
+        self.attr = attr
+
+    def __get__(self, obj, cls):
+        if obj is None:
+            return self
+        if self.attr in obj.__dict__:
+            return obj.__dict__[self.attr]
+        raise AttributeError('Атрибут не найден')
+
+    def __set__(self, obj, val):
+        if not hasattr(obj, '_versions'):
+            obj.__dict__['_versions'] = []
+        obj.__dict__['_versions'].append(val)
+        obj.__dict__[self.attr] = val
+
+    def get_version(self, obj, n):
+        return obj.__dict__['_versions'][n - 1]
+
+    def set_version(self, obj, n):
+        obj.__dict__[self.attr] = obj.__dict__['_versions'][n - 1]
+
+
+class Student:
+    age = Versioned()
+
+
+student1 = Student()
+student2 = Student()
+
+student1.age = 18
+student1.age = 19
+student1.age = 20
+
+student2.age = 30
+student2.age = 31
+student2.age = 32
+
+print(student1.age)
+print(student2.age)
+Student.age.set_version(student1, 1)
+Student.age.set_version(student2, 2)
+print(student1.age)
+print(student2.age)
